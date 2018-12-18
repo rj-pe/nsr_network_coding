48be3bce5774fb8ef29182a508b743aa2ab5b99b
rj <rjpe@protonmail.com>
2018-12-18 17:08:45 -0500

    Implemented Inheritance for classes based on ``IntermediateNode.java``. Improved the readability of local encoding vector logging.

--- src/App.java
+++ src/App.java
@@ -35,16 +35,16 @@ public class App {
             List<Node> sinkNodes = new ArrayList<>();
             sinkNodes.add(new SinkNode(ff24, "t1", 3));
             sinkNodes.add(new SinkNode(ff24, "t2", 3));
-            int[] l1 = {14, 5, 7};
-            int[] l2 = {6, 4, 13};
-            int[] l3 = {0, 1, 0};
+            int[] l1 = {13, 11, 9};
+            int[] l2 = {4, 3, 11};
+            int[] l3 = {5, 11, 5};
             intermediateNodes.add(new IntermediateNodeDebug(sinkNodes, ff24, "i1", l1, 3));
             intermediateNodes.add(new IntermediateNodeDebug(sinkNodes, ff24, "i2", l2, 3));
             intermediateNodes.add(new IntermediateNodeDebug(sinkNodes, ff24, "i3", l3, 3));
             SenderNodeDebug sender = new SenderNodeDebug(intermediateNodes, ff24, args[0], 100, 3);
             sender.handle();
             /// end debug
-        } else {
+        } else { // Production Mode
             // Create the network over which the packets will be sent.
             // Network parameters should be specified when creating the network object.
             Topology network = new Topology(3, 5, 4, ff24);
